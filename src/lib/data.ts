491a1a8ca910b7b94d1ffb3fcde056dca5ef876c
Chirag Bhadarka <chiragbhadarka1204@gmail.com>
2025-09-21 07:50:49 +0000

    Delete inside this section  (_for element h3_)

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -559,17 +559,10 @@ export const resources: Resource[] = [
         link: 'https://youtube.com/playlist?list=PL3qvHcrYGy1v2kJX4SSsurE3_GdVe0ZD5&si=-5B4Y0wPuV1ov9lP',
         category: 'Video Playlist',
         subject: 'Physics'
-    },
-    {
-        id: '8',
-        title: 'Basic Electrical Engineering (BEE) by Atul Prakashan',
-        description: 'A textbook for first-year engineering students, covering the fundamentals of electrical engineering.',
-        link: 'https://drive.google.com/file/d/1QbjtPIdu96yqUOZpCs3Zt1hm11nJGDou/view?usp=drivesdk',
-        category: 'Textbook',
-        subject: 'Electrical'
     }
 ]
 
     
 
     
+

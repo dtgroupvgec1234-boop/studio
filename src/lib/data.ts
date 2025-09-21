5e941df41555c828abb35a72b8e6e230eed86d6b
Chirag Bhadarka <chiragbhadarka1204@gmail.com>
2025-09-21 07:52:48 +0000

    Inside this section add one book name BEE author name is atul (_for elem

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -559,10 +559,19 @@ export const resources: Resource[] = [
         link: 'https://youtube.com/playlist?list=PL3qvHcrYGy1v2kJX4SSsurE3_GdVe0ZD5&si=-5B4Y0wPuV1ov9lP',
         category: 'Video Playlist',
         subject: 'Physics'
+    },
+    {
+        id: '8',
+        title: 'BEE by Atul Prakashan',
+        description: 'A textbook for Basic Electrical Engineering.',
+        link: '#',
+        category: 'Textbook',
+        subject: 'Basic Electrical Engineering'
     }
 ]
 
     
 
     
+
 

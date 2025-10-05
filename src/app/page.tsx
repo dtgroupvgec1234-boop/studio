5c78d9b816dc33c633018b31e4a4ab1432ce1f53
Chirag Bhadarka <chiragbhadarka1204@gmail.com>
2025-10-05 15:32:18 +0000

    And add name CHIRAG BHADARAKA, HET BORADA,RUDRA BALDHA ,LASHKARI DENISH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,6 +170,9 @@ export default function Home() {
                 <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
                   This website was created by :
                 </p>
+                <p className="mx-auto max-w-[700px] text-accent-foreground md:text-xl font-semibold">
+                  CHIRAG BHADARAKA, HET BORADA, RUDRA BALDHA, LASHKARI DENISH
+                </p>
               </div>
             </div>
           </div>
